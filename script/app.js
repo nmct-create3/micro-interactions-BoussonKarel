6ee1336e493078eed2724d073bd6b5e8395cef2e
BoussonKarel <BoussonKarel@users.noreply.github.com>
2020-10-06 11:48:37 +0200

    Password toggle

--- script/app.js
+++ script/app.js
@@ -1,12 +1,26 @@
+let passwordInput, passwordCheckbox;
 function handleFloatingLabel() {
    
 }
 
 function handlePasswordSwitcher() {
-    
+    // Haal in deze functie de password-input en de password-toggle op. Doe dit met .js- prefixed classnames.
+    passwordCheckbox.addEventListener("input", function() {
+        if (passwordCheckbox.checked) {
+            passwordInput.type = "text";
+        }
+        else {
+            passwordInput.type = "password";
+        }
+        console.log("checked");
+    });
+    // We gaan luisteren of er geklikt wordt op de checkbox.
+    // Als er geklikt wordt, veranderen we het type van de input van 'password' naar 'text' en vice versa.
 }
 
 document.addEventListener('DOMContentLoaded', function () {
+    passwordInput = document.querySelector(".js-password-input");
+    passwordCheckbox = document.querySelector(".js-password-checkbox");
     console.log('Script loaded!');
     handleFloatingLabel();
     handlePasswordSwitcher();
